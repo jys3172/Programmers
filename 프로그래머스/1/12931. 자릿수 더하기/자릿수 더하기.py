8823026a595a23e39936da88c72d039e179eaea6
jys3172 <145959870+jys3172@users.noreply.github.com>
2024-06-02 02:24:35 +0900

    자릿수 더하기.py

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -2,6 +2,7 @@ def solution(n):
     answer = 0
     while n >0:
         answer += n % 10
-        n //= 10
+        n //= 10 -> 나누기 확ㅇ니 잘하기
 
     return answer
+    -> 전반적인 오류는 while문은 잘 사용했지만 내부에서 값을 아예 나누는 생각을 못했음.
