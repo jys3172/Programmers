70994de3e5605ddf5b76002432016374b1d8e20d
jys3172 <145959870+jys3172@users.noreply.github.com>
2024-06-03 00:13:33 +0900

    자연수 뒤집어 배열로 만들기.py

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = []
-    s = reversed(str(n))
+    s = reversed(str(n)) -> reversed함수 기억하기. 그리고 입력이 12345이므로 n을 string화
     for i in s:
-        answer.append(int(i))
+        answer.append(int(i)) -> s는 string이므로 거꾸로 된 첫번째부터 int로 가져옴
     return answer
