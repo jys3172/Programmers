a2a79363243cc392c00b298b34f732dd62e8811b
jys3172 <145959870+jys3172@users.noreply.github.com>
2024-06-05 02:13:32 +0900

    가운데 글자 가져오기.py

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,6 +1,6 @@
 def solution(s):
     answer =''
-    center = int(len(s)/2)
+    center = int(len(s)/2)    -> len()을 사용한다고 형식이 변하지 않는다. 정확히 int를 이용한 형변환
     if len(s) % 2 !=0:
         answer = s[center]        
     else:
