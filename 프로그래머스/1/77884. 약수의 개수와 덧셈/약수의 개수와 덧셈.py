74c3d570a22c43984c332d6df281c93acc5f0002
jys3172 <145959870+jys3172@users.noreply.github.com>
2024-06-13 02:26:09 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.89 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,12 @@
+def solution(left, right):
+    answer = 0;
+    for i in range(left,right+1):
+        remain = 0;
+        for j in range(1,i+1):
+            if(i%j==0):
+                remain +=1
+        if (remain%2==0):
+            answer = answer + i
+        else:
+            answer = answer -i
+    return answer
