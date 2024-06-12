0eab05561fa8316beb388eb90dff1567cd5dca09
jys3172 <145959870+jys3172@users.noreply.github.com>
2024-06-13 02:26:48 +0900

    Update 약수의 개수와 덧셈.py

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,7 +1,7 @@
 def solution(left, right):
     answer = 0;
     for i in range(left,right+1):
-        remain = 0;
+        remain = 0;        -> 초기화 장소를 잘 확인하자
         for j in range(1,i+1):
             if(i%j==0):
                 remain +=1
