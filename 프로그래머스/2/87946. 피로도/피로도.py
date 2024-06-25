33182304f606e85d6ce947d8c71f1dd9bdc47762
jys3172 <145959870+jys3172@users.noreply.github.com>
2024-06-25 19:53:02 +0900

    피로도.py

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,18 @@
-from itertools import permutations
+from itertools import permutations    # itertools의 수열을 사용하기 위한 전처리
 
 
 def solution(k, dungeons):
     answer = 0
     
-    for p in permutations(dungeons, len(dungeons)):
-        fatigue = k
-        cnt = 0
+    for p in permutations(dungeons, len(dungeons)):        # 수열(리스트명, 리스트 내 원자 수)
+        fatigue = k                                        # 현재 피로도
+        cnt = 0                                            # 던전 횟수
         
-        for need, spend in p:
-            if fatigue >= need:
-                fatigue -= spend
-                cnt += 1
-        answer = max(answer,cnt)
+        for need, spend in p:                              # 반복 필요피로도, 사용피로도가 p만큼 존재
+            if fatigue >= need:                            # 조건 필요피로도가 피로도보다작을때
+                fatigue -= spend                           # 사용피로도를 빼줌
+                cnt += 1                                   # 던전횟수 1 증가
+        answer = max(answer,cnt)                           # 반복을 통해서 던전 한 횟수가 최대가 되는 경우의 수를 구함
     
     
     return answer
